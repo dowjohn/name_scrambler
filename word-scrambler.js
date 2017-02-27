52d7745213700a725b447b18f13bb6ef5644790c
John Dow <jldjld503@gmail.com>
2017-02-27 12:59:09 -0600

    small edits to trim spaces and return actual letters

--- word-scrambler.js
+++ word-scrambler.js
@@ -1,43 +1,41 @@
-var lettersArray = new Array();
-var prompt = require('prompt');
-var resultWord1 = new String();
-var resultWord2 = new String();
-prompt.start();
+var prompt = require('prompt')
+var resultWord1
+prompt.start()
 
-prompt.get(['word1', 'word2'], function (err, result) {
-  resultWord1 = result.word1;
-  resultWord2 = result.word2;
-  console.log(scramble(concatMultipleStringsIntoArray(resultWord1, resultWord2)));
-});
-// console.log (scramble(resultWord1, resultWord2));
-
-function scramble(arrayOfLetters) {
-  var lettersArray = arrayOfLetters;
-  var outArray = new Array(lettersArray.length);
-  for (i = 0; i < outArray.length; i++) {
-    letterInt = numGen(lettersArray);
-    outArray[i] = lettersArray[letterInt];
-    lettersArray.splice(letterInt, 1);
+prompt.get(['word1'], function (err, result) {
+  resultWord1 = result.word1.trim().toLowerCase()
+  console.log(scramble(concatMultipleStringsIntoArray(resultWord1)))
+  if (err != null) {
+    console.log(err)
   }
-  outArray.splice(Math.floor(Math.random() * outArray.length), 1, " ");
-  return outArray.join("");
+})
+
+function scramble (arrayOfLetters) {
+  var lettersArray = arrayOfLetters
+  var outArray = new Array(lettersArray.length)
+  var i
+  for (i = 0; i < outArray.length; i++) {
+    var letterInt = numGen(lettersArray)
+    outArray[i] = lettersArray[letterInt]
+    lettersArray.splice(letterInt, 1)
+  }
+  outArray.splice(Math.floor(Math.random() * outArray.length), 0, ' ')
+  return outArray.join('').trim()
 }
 
-function numGen(letters) {
-  return Math.floor(Math.random() * letters.length);
+function numGen (letters) {
+  return Math.floor(Math.random() * letters.length)
 }
 
 // takes as many strings as inputted
-function concatMultipleStringsIntoArray(/**/) {
-  var args = arguments;
-  var arrayOfCharacters = new Array();
-  for(var i=0; i<args.length; i++){
-    var wordSplit = args[i].split("")
-    var word = args[i];
+function concatMultipleStringsIntoArray (/**/) {
+  var args = arguments
+  var arrayOfCharacters = []
+  for (var i = 0; i < args.length; i++) {
+    var wordSplit = args[i].split('')
     wordSplit.forEach(function (letter) {
-      arrayOfCharacters.push(letter);
-    });
-    // arrayOfCharacters.concat(word.split(""));
+      arrayOfCharacters.push(letter)
+    })
   }
-  return arrayOfCharacters;
+  return arrayOfCharacters
 }
